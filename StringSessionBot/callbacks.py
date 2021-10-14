8e866e86f83258f20dd1909dd61bf9d7ac2c8cd7
sahildatax <84317311+sahildatax@users.noreply.github.com>
2021-10-14 16:29:54 +0530

    Update callbacks.py

--- StringSessionBot/callbacks.py
+++ StringSessionBot/callbacks.py
@@ -43,7 +43,7 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
         )
     elif query == "generate":
         await callback_query.message.reply(
-            "Silahkan Pilih String Mana Yang Ingin Kamu Ambil 😁",
+            "Please Choose Which String You Want To Generate",
             reply_markup=InlineKeyboardMarkup([[
                 InlineKeyboardButton("Pyrogram", callback_data="pyrogram"),
                 InlineKeyboardButton("Telethon", callback_data="telethon")
